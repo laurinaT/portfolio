2a5138940b525168f672d2bf106b5b8dc822e504
Laurina Toussaint <laurinatoussaint@gmail.com>
2021-11-24 15:24:04 +0100

    First commit

---
+++ src/layout/Header.js
@@ -0,0 +1,17 @@
+import Logo from "../img/logo.png"
+
+
+function Header() {
+	return(
+		<header className="bg-white row py-5 align-items-center justify-content-center">
+			<div className="col-6 row align-items-center justify-content-center text-center">
+				<div className="col-4"><img className="img-fluid" src={Logo} alt={"logo"}/></div>
+				<div><h1 className="text-primary">Laurina TOUSSAINT</h1></div>
+
+				<div><h2 className="text-primary">Développeuse Web</h2></div>
+			</div>
+		</header>
+	)
+}
+
+export default Header;
